12b3a6b34a7e0d686e6df2e8ede3963229df3e9e
Miguel Quiroz <110549106+DEV1lmig@users.noreply.github.com>
2024-11-26 03:22:23 +0000

    register change

--- flask/models.py
+++ flask/models.py
@@ -32,7 +32,7 @@ class Usuario(db.Model):
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(1024), nullable=False)
     role = db.Column(db.String(80), nullable=False)
 
     def set_password(self, password):
